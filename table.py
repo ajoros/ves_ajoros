a233321b425cd0b6c0492f051a30b24a331abfcc
vitale232 <vitale232@gmail.com>
2015-12-14 13:56:12 -0800

    Remove dev code

--- table.py
+++ table.py
@@ -12,7 +12,6 @@ class PaletteTableModel(QAbstractTableModel):
     def __init__(self, table=[[]], headers=[], colors=[], parent=None):
 
         QAbstractTableModel.__init__(self, parent)
-        # super(self, colors).__init__(parent)
 
         self.__table = table
         self.__headers = headers
@@ -54,14 +53,8 @@ class PaletteTableModel(QAbstractTableModel):
         row = index.row()
         column = index.column() - 1
 
-        # if column == 0:
-        #     pass
-
         if role == Qt.EditRole:
 
-            # if column == 0:
-            #     return self.__colors[row][column]
-            # else:
             return self.__table[row][column]
 
         if role == Qt.ToolTipRole:
@@ -76,21 +69,6 @@ class PaletteTableModel(QAbstractTableModel):
 
             return value
 
-        # if role == Qt.DecorationRole:
-        #     row = index.row()
-        #     column = index.column()
-        #     # if column == 0:
-        #     #     # value = self.__colors[row][column]
-        #     #     value = self.__colors[row][1]
-
-        #     #     pixmap = QPixmap(26, 26)
-        #     #     pixmap.fill(value)
-
-        #     #     icon = QIcon(pixmap)
-
-        #     #     return icon
-        #     return ''
-
 
     def setData(self, index, value, role=Qt.EditRole):
 
@@ -102,17 +80,6 @@ class PaletteTableModel(QAbstractTableModel):
                 self.__table[row][column] = value
                 self.dataChanged.emit(index, index)
                 return True
-            # color = QColor(value)
-
-            # if color.isValid():
-            #     if column == 0:
-            #         self.__colors[row][column] = value
-            #         self.dataChanged.emit(index, index)
-            #         return True
-            #     else:
-            #         self.__colors[row][column] = color
-            #         self.dataChanged.emit(index, index)
-            #         return True
         return False
 
 
@@ -131,31 +98,11 @@ class PaletteTableModel(QAbstractTableModel):
 
             if orientation == Qt.Vertical:
 
-                # validSections = range(2, rowCount, 4)
-
-                # try:
-                    # index = colors.index(section)
-                    # print(index)
-                    # if column == 0:
-                        # value = self.__colors[row][column]
                 value = self.__colors[section]
                 pixmap = QPixmap(25, 25)
                 pixmap.fill(QColor(value))
 
                 return pixmap
-                # except:
-                #     pixmap = QPixmap(26, 50)
-                #     pixmap.fill(QColor(value))
-
-                #     # icon = QIcon(pixmap)
-                #     return pixmap
-                # except:
-                #     pixmap = QPixmap(0, 0)
-                #     return pixmap
-
-        # else:
-        #     return QAbstractTableModel.headerData(
-        #         self, section, orientation, role)
 
 
     def insertRows(self, position, rows, parent=QModelIndex()):
@@ -173,17 +120,6 @@ class PaletteTableModel(QAbstractTableModel):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    # import matplotlib.pyplot as plt
-    # plt.style.use('bmh')
-    # x = range(10)
-    # y = range(11, 21)
-    # for i in range(len(colors[::4])):
-    #     z = [z + i for z in y]
-    #     plt.plot(x, z, color=colors[::4][i])
-    #     plt.plot(x, z[::-1], color=colors[::4][::-1][i])
-    #     del z
-    # plt.show()
 
     model = PaletteTableModel(tableData, headers, colors)
 

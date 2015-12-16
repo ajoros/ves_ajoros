991591309ebf5106aa4c36cc5f47a90fa4c3889d
vitale232 <vitale232@gmail.com>
2015-12-16 07:56:31 -0800

    Start writing initUi method with a menu - breaks window displaying

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 import sys
 
+import matplotlib
+matplotlib.use('Qt5Agg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction, QApplication
 from PyQt5.uic import loadUiType
 
 
@@ -18,13 +21,32 @@ class Main(QMainWindow, UI_MainWindow):
     def __init__(self):
 
         super(Main, self).__init__()
-        self.setupUi(self)
+        print(self)
+        print(dir(self))
+        print(self.initUi)
+        self.initUi()
+
+
+    def initUi(self):
+
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self) 
+        exitAction.setShortcut('Alt+F4')
+        # exitAction.setNativeMenuBar(False)
+
+        exitAction.triggered.connect(QApplication.quit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     main = Main()
+    # print(dir(main))
     main.show()
 
     sys.exit(app.exec_())

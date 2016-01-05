9ecd879fe33b3197a4ebdeabd416ce01ed9a48f6
vitale232 <vitale232@gmail.com>
2016-01-05 07:34:40 -0800

    Convert aggregate to a function

--- aggregate.py
+++ aggregate.py
@@ -2,40 +2,46 @@ import copy
 
 import numpy as np
 
-from templates.tempData import (
-    columnCount, columns, colors, headers, rowCount, tableData)
+from templates.tempData import rowCount, tableData
 
 
-temporaryTableData = copy.deepcopy(tableData)
-# Convert values from Qt model strings to floats or np.nan
-for i in range(rowCount):
-    row = temporaryTableData[i]
-    for j in range(len(row)):
-        value = row[j]
+def aggregateTable(tableData, rowCount):
+
+    temporaryTableData = copy.deepcopy(tableData)
+    # Convert values from Qt model strings to floats or np.nan
+    for i in range(rowCount):
+        row = temporaryTableData[i]
+        for j in range(len(row)):
+            value = row[j]
+            try:
+                value = float(value)
+            except ValueError:
+                value = np.nan
+            row[j] = value
+            temporaryTableData[i] = row
+
+    # Extract the raw current and voltage values, pass on Index error
+    voltage, current, voltageSpacing = [], [], []
+    for row in temporaryTableData[:rowCount]:
         try:
-            value = float(value)
-        except ValueError:
-            value = np.nan
-        row[j] = value
-        temporaryTableData[i] = row
+            voltageSpacing.append(row.pop(0))
+            voltage.append(row.pop(2))
+            current.append(row.pop(2))
+        except (IndexError, ValueError):
+            pass
 
-# Extract the raw current and voltage values, pass on Index error
-voltage, current, voltageSpacing = [], [], []
-for row in temporaryTableData[:rowCount]:
-    try:
-        voltageSpacing.append(row.pop(0))
-        voltage.append(row.pop(2))
-        current.append(row.pop(2))
-    except (IndexError, ValueError):
-        pass
+    # Convert the lists of voltage and current to Numpy arrays
+    voltageSpacing = np.array(voltageSpacing[::4], dtype=np.float64)
+    voltage = np.array(voltage, dtype=np.float64).reshape(rowCount / 4, 4)
+    current = np.array(current, dtype=np.float64).reshape(rowCount / 4, 4)
 
-# Convert the lists of voltage and current to Numpy arrays
-voltageSpacing = np.array(voltageSpacing[::4], dtype=np.float64)
-voltage = np.array(voltage, dtype=np.float64).reshape(rowCount / 4, 4)
-current = np.array(current, dtype=np.float64).reshape(rowCount / 4, 4)
+    meanVoltage = np.mean(voltage, axis=1)
+    meanCurrent = np.mean(current, axis=1)
 
-meanVoltage = np.mean(voltage, axis=1)
-meanCurrent = np.mean(current, axis=1)
+    return (voltageSpacing, meanVoltage, meanCurrent)
+
+
+voltageSpacing, meanVoltage, meanCurrent = aggregateTable(tableData, rowCount)
 
 
 if __name__ == '__main__':

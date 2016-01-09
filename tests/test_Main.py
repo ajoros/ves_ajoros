eee72f117af2b9ea4e7f5754c43a6a2e41625854
vitale232 <vitale232@gmail.com>
2016-01-09 08:56:50 -0800

    Break all of the tests with new tableData

--- tests/test_Main.py
+++ tests/test_Main.py
@@ -2,13 +2,12 @@ import os
 import sys
 import unittest
 
-import numpy as numpy
+import numpy as np
 from numpy.testing import assert_array_almost_equal
 from PyQt5.QtWidgets import QApplication
 
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from templates.tempData import tableData, headers, colors
+from templates.tempData import headers, colors
 from main import Main
 
 
@@ -18,22 +17,19 @@ class TestMain(unittest.TestCase):
 
         # Set up a mock set of tableData similar to the Qt table with 3 rows
         self.tableData = [
-            ['0.6', '0.3', '0.9', '122', '82'],
-            ['', '', '', '120', '82'],
-            ['', '', '', '254', '82'],
-            ['', '', '', '125', '83'],
-            ['1', '0.5', '1.5', '156', '82'],
-            ['', '', '', '42', '83'],
-            ['', '', '', '151', '80'],
-            ['', '', '', '38', '81'],
-            ['1.6', '0.8', '2.4', '106', '83'],
-            ['', '', '', '-10', '82'],
-            ['', '', '', '91', '80'],
-            ['', '', '', '-11', '82']]
+            ['0.6', '0.3', '0.9', '5750', '23'],
+            ['', '', '', '5930', '24'],
+            ['', '', '', '5850', '24'],
+            ['', '', '', '5950', '25'],
+            ['1', '0.5', '1.5', '3650', '30'],
+            ['', '', '', '3690', '30'],
+            ['', '', '', '3670', '30'],
+            ['', '', '', '3710', '30'],
+            ['1.6', '0.8', '2.4', '1462', '31']]
         self.rowCount = len(self.tableData)
         self.app = QApplication([])
         self.main = Main(
-            tableData[:-2], headers, colors, (0, 0.5), 50, 50, angle=0.)
+            self.tableData, headers, colors, (0, 0.5), 50, 50, angle=0.)
 
 
     def tearDown(self):
@@ -41,13 +37,17 @@ class TestMain(unittest.TestCase):
         del self.app, self.tableData, self.main, self.rowCount
 
 
-    def test_Main_compute_schlumberger(self):
+    def test_Main_compute(self):
 
-        self.main.wenner()
+        self.main.schlumberger()
+        self.main.compute()
 
         assert_array_almost_equal(
             self.main.apparentResistivity,
             np.array([409.80330837, 300.5875851]))
+
+
+
 
 
 

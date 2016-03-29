6a7973abc3c856f0963364e6170fd933d97251bd
vitale232 <vitale232@gmail.com>
2016-03-29 06:31:31 -0700

    Start pimping out some code

--- ves/ves_inverse.py
+++ ves/ves_inverse.py
@@ -1,36 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 28 16:32:48 2016
-
-@author: jclark
-
-this code uses the Ghosh method to determine the apparent resistivities
-for a layered earth model. Either schlumberger or Wenner configurations
-can be used
-"""
-
+import os
+import sys
+import random # not for prod
 
 import numpy as np
-import random
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-import sys
+
+os.chdir('./templates') # not for prod
+print(os.getcwd())
+print(os.listdir())
+from templates.tempData import coefficients
+
+schlumbergerFilterCoefficients, wennerFilterCoefficients = coefficients
 
 # Schlumberger filter
-fltr1 = [0., .00046256, -.0010907, .0017122, -.0020687,
-         .0043048, -.0021236, .015995, .017065, .098105, .21918, .64722,
-         1.1415, .47819, -3.515, 2.7743, -1.201, .4544, -.19427, .097364,
-         -.054099, .031729, -.019109, .011656, -.0071544, .0044042,
-         -.002715, .0016749, -.0010335, .00040124]
+# fltr1 = [0., .00046256, -.0010907, .0017122, -.0020687,
+#          .0043048, -.0021236, .015995, .017065, .098105, .21918, .64722,
+#          1.1415, .47819, -3.515, 2.7743, -1.201, .4544, -.19427, .097364,
+#          -.054099, .031729, -.019109, .011656, -.0071544, .0044042,
+#          -.002715, .0016749, -.0010335, .00040124]
 
-#Wenner Filter
-fltr2 = [0., .000238935, .00011557, .00017034, .00024935,
-         .00036665, .00053753, .0007896, .0011584, .0017008, .0024959,
-         .003664, .0053773, .007893, .011583, .016998, .024934, .036558,
-         .053507, .078121, .11319, .16192, .22363, .28821, .30276, .15523,
-         -.32026, -.53557, .51787, -.196, .054394, -.015747, .0053941,
-         -.0021446, .000665125]
+# #Wenner Filter
+# fltr2 = [0., .000238935, .00011557, .00017034, .00024935,
+#          .00036665, .00053753, .0007896, .0011584, .0017008, .0024959,
+#          .003664, .0053773, .007893, .011583, .016998, .024934, .036558,
+#          .053507, .078121, .11319, .16192, .22363, .28821, .30276, .15523,
+#          -.32026, -.53557, .51787, -.196, .054394, -.015747, .0053941,
+#          -.0021446, .000665125]
+
 
 #I know there must be a better method to assign lists. And probably numpy
 #arrays would be best. But my Python wasn't up to it. If the last letter
@@ -77,6 +75,7 @@ errmin = 1.e10
 
 # INPUT
 index = 2   # 1 is for shchlumberger and 2 is for Wenner
+arrayType = 'schlumberger'
 e = 3   #number of layers
 n = 2*e-1
 
@@ -111,10 +110,6 @@ small[4] = 2.
 xlarge[4] = 100
 small[5] = 500.
 xlarge[5] = 3000.
-
-print(small)
-s = input('$:')
-print(s)
 
 iter_ = 10000  #number of iterations for the Monte Carlo guesses. to be input on GUI
 
@@ -153,7 +148,7 @@ def transf(y, i):
         b = (1. - a) / (1. + a)
         rs = p[n + 1 - j]
         tpr = b * rs
-        t[j] = (tpr + t[j-1]) / (1. + tpr * t[j-1] / (rs * rs))
+        t[j] = (tpr + t[j - 1]) / (1. + tpr * t[j - 1] / (rs * rs))
     r[i] = t[e]
     return
 
@@ -166,14 +161,14 @@ def filters(b, k):
     return
 
 def rmsfit():
-    if index == 1:
+    if arrayType.lower() == 'schlumberger':
         y = spac - 19. * delx - 0.13069
         mum1 = m + 28
         for i in range(1, mum1 + 1, 1):
             transf(y, i)
             y = y + delx
-        filters(fltr1, 29)
-    elif index == 2:
+        filters(schlumbergerFilterCoefficients, 29)
+    elif arrayType.lower() == 'wenner':
         s = np.log(2.)
         y = spac - 10.8792495 * delx
         mum2 = m + 33
@@ -184,7 +179,7 @@ def rmsfit():
             transf(y1, i)
             r[i] = 2. * a - r[i]
             y = y + delx
-        filters(fltr2, 34)
+        filters(wennerFilterCoefficients, 34)
     else:
         print(" type of survey not indicated")
         sys.exit()
@@ -223,14 +218,18 @@ def spline(n, yp1, ypn, x=[] ,y=[] ,y2=[]):
         y2[i] = (sig-1.) / p
         u[i] = (
             ((6. * ((y[i + 1] - y[i]) / (x[i + 1] - x[i]) - (y[i] - y[i-1]) /
-              x[i] - x[i-1])) / (x[i + 1] - x[i - 1]) - sig * u[i - 1]) / p)
+              x[i] - x[i-1])) / ( x[i + 1] - x[i - 1]) - sig * u[i - 1]) / p )
 
     if ypn > one29:
         qn = 0.
         un = 0.
     else:
         qn = 0.5
-        un = (3. / (x[n] - x[n - 1])) * (ypn - (y[n] - y[n - 1]) / (x[n] - x[n - 1]))
+        un = (
+            (3. / (x[n] - x[n - 1])) *
+            (ypn - (y[n] - y[n - 1]) /
+            (x[n] - x[n - 1]))
+            )
 
     y2[n] = (un - qn * u[n - 1]) / (qn * y2[n - 1] + 1.)
     for k in range(n-1, -1, -1):
@@ -254,62 +253,60 @@ def splint(n, x ,xa=[], ya=[], y2a=[]):
     a = (xa[khi] - x) / h
     b = (x - xa[klo]) / h
     y = (a * ya[klo] + b * ya[khi] + ((a * a * a - a) * y2a[klo] +
-                (b * b * b - b) * y2a[khi]) * (h * h) /6.)
+        (b * b * b - b) * y2a[khi]) * (h * h) /6.)
 
     return y
 
 
-#main here
-if __name__ == '__main__':
 
-    readData()
-    print(adat[1:ndat],rdat[1:ndat])
-    print('log stufffff')
+readData()
+print(adat[1:ndat],rdat[1:ndat])
+print('log stufffff')
 
-    print(adatl[1:ndat],rdatl[1:ndat])
-    for iloop in range(1, iter_ + 1):
-        #print( '  iloop is ', iloop)
-        for i in range(1, n + 1):
-            randNumber = random.random()
-            #print(randNumber, '  random')
-            p[i] = (xlarge[i] - small[i]) * randNumber + small[i]
+print(adatl[1:ndat],rdatl[1:ndat])
+for iloop in range(1, iter_ + 1):
+    #print( '  iloop is ', iloop)
+    for i in range(1, n + 1):
+        randNumber = random.random()
+        #print(randNumber, '  random')
+        p[i] = (xlarge[i] - small[i]) * randNumber + small[i]
 
-        rms = rmsfit()
+    rms = rmsfit()
 
-        if rms < errmin:
-            print('rms  ', rms, '   errmin ', errmin)
-            for i in range(1,n+1,1):
-                pkeep[i] = p[i]
-            for i in range(1, m+1, 1):
-                rkeep[i] = r[i]
-                rkeepl[i] = rl[i]
-            for i in range(1,ndat+1,1):
-                pltanswerkeepl[i] = pltanswerl[i]
-                pltanswerkeep[i] = pltanswer[i]
-            errmin = rms
+    if rms < errmin:
+        print('rms  ', rms, '   errmin ', errmin)
+        for i in range(1,n+1,1):
+            pkeep[i] = p[i]
+        for i in range(1, m+1, 1):
+            rkeep[i] = r[i]
+            rkeepl[i] = rl[i]
+        for i in range(1,ndat+1,1):
+            pltanswerkeepl[i] = pltanswerl[i]
+            pltanswerkeep[i] = pltanswer[i]
+        errmin = rms
 
 #output the best fitting earth model
-    print(' Layer ', '     Thickness  ', '   Res_ohm-m  ')
-    for i in range(1,e,1):
-        print(i, pkeep[i], pkeep[e+i-1])
+print(' Layer ', '     Thickness  ', '   Res_ohm-m  ')
+for i in range(1,e,1):
+    print(i, pkeep[i], pkeep[e+i-1])
 
-    print( e, '  Infinite ', pkeep[n])
-    for i in range(1,m+1, 1):
-        asavl[i] = np.log10(asav[i])
+print( e, '  Infinite ', pkeep[n])
+for i in range(1,m+1, 1):
+    asavl[i] = np.log10(asav[i])
 
 #output the error of fit
-    print( ' RMS error   ', errmin)
-    print( '  Spacing', '  Res_pred  ', ' Log10_spacing  ', ' Log10_Res_pred ')
-    for i in range(1,m+1,1):
-        #print(asav[i], rkeep[i], asavl[i], rkeepl[i])
-        print("%7.2f   %9.3f  %9.3f  %9.3f" % ( asav[i], rkeep[i],
-              asavl[i], rkeepl[i]))
+print( ' RMS error   ', errmin)
+print( '  Spacing', '  Res_pred  ', ' Log10_spacing  ', ' Log10_Res_pred ')
+for i in range(1,m+1,1):
+    #print(asav[i], rkeep[i], asavl[i], rkeepl[i])
+    print("%7.2f   %9.3f  %9.3f  %9.3f" % ( asav[i], rkeep[i],
+          asavl[i], rkeepl[i]))
 
-    plt.loglog(asav[1:m],rkeep[1:m],'-')  # resistivity prediction curve
-    plt.loglog(adat[1:ndat],pltanswerkeep[1:ndat], 'ro')  # predicted data red dots
-    s=7
-    plt.loglog(adat[1:ndat],rdat[1:ndat],'bo',markersize=s) #original data blue dots
-    plt.show()
-    plt.grid(True)
-    sys.exit(0)
+plt.loglog(asav[1:m], rkeep[1:m], '-')  # resistivity prediction curve
+plt.loglog(adat[1:ndat], pltanswerkeep[1:ndat], 'ro') # predicted data red dots
+s=7
+plt.loglog(adat[1:ndat], rdat[1:ndat], 'bo', markersize=s)#originaldatabluedots
+plt.show()
+plt.grid(True)
+sys.exit(0)
 
